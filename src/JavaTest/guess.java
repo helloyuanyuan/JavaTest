e917421ff12e7c94669ad096db722610ec1519e0
helloyuanyuan <minidisk@vip.qq.com>
2015-05-25 14:06:44 +0800

    20150525 commit 01

---
+++ src/JavaTest/guess.java
@@ -0,0 +1,32 @@
+package JavaTest;
+
+import java.util.Scanner;
+
+public class guess {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+
+		int number = (int) (Math.random() * 100 + 1);
+		int guess;
+		int count = 0;
+
+		System.out.println("Please input a number: ");
+
+		do {
+			guess = in.nextInt();
+			count++;
+
+			if (guess < number) {
+				System.out.println("less");
+			} else if (guess > number) {
+				System.out.println("greater");
+			}
+		} while (number != guess);
+
+		System.out.println("Bingo! The number is: " + number + ". Spent " + count + " times.");
+
+	}
+
+}
