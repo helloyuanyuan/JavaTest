6cbb67169284d11e2d185510b46c1b1cd77d1a49
helloyuanyuan <minidisk@vip.qq.com>
2015-05-25 14:11:04 +0800

    20150525 commit 02

--- src/JavaTest/guess.java
+++ src/JavaTest/guess.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class guess {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
