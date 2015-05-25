849db9d1f6bc74cef171d3598b44daa942aa80d6
helloyuanyuan <minidisk@vip.qq.com>
2015-05-25 23:51:17 +0800

    20150525 commit 09

---
+++ src/JavaTest/随机质数.java
@@ -0,0 +1,30 @@
+package JavaTest;
+
+public class Ëæ»úÖÊÊı {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int random = (int)((Math.random())*100+1);
+		boolean test = isPrime(random);
+		System.out.println(random+" is "+test);
+
+	}
+
+	public static boolean isPrime(long n) {
+		if (n <= 3) {
+			return n > 1;
+		}
+		if (n % 2 == 0 || n % 3 == 0) {
+			return false;
+		}
+
+		for (int i = 5; i * i <= n; i += 6) {
+			if (n % i == 0 || n % (i + 2) == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
