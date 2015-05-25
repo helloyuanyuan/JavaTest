8217cb4a0f83ecf116998ed226f708bb0d8d0384
helloyuanyuan <minidisk@vip.qq.com>
2015-05-25 20:33:17 +0800

    20150525 commit 06

--- src/JavaTest/temp.java
+++ src/JavaTest/temp.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class temp {
 
-	@SuppressWarnings("resource")
+	@SuppressWarnings({ "resource", "unused" })
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
