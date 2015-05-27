1f0f3ac7a1b3e01866f6678ccd6a86a32af6c640
helloyuanyuan <minidisk@vip.qq.com>
2015-05-27 16:39:07 +0800

    20150527 commit

--- src/JavaTest/A0020_银行账号.java
+++ src/JavaTest/A0020_银行账号.java
@@ -4,34 +4,44 @@ public class A0020_银行账号 {
 
 	public static void main(String[] args) {
 		A0020_银行账号 test = new A0020_银行账号();
-		test.method();
+		test.routingNumber();
 	}
 
-	public void method() {
+	public void routingNumber() {
 
-		// Bank Info
-		String rnd = String.format("%08d", new java.util.Random().nextInt(99999999));
-		int rt = Integer.parseInt(rnd);
+		String random = String.format("%08d", new java.util.Random().nextInt(99999999));
+		int rt = Integer.parseInt(random);
+		int length = 10000000;
+		int n = 0;
+		int tmp;
+		int n9;
 
-		int n1 = rt % 10 * 7;
-		rt = rt / 10;
-		int n2 = rt % 10 * 3;
-		rt = rt / 10;
-		int n3 = rt % 10 * 1;
-		rt = rt / 10;
-		int n4 = rt % 10 * 7;
-		rt = rt / 10;
-		int n5 = rt % 10 * 3;
-		rt = rt / 10;
-		int n6 = rt % 10 * 1;
-		rt = rt / 10;
-		int n7 = rt % 10 * 7;
-		rt = rt / 10;
-		int n8 = rt % 10 * 3;
-		rt = rt / 10;
-		int n9 = 10 - (n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8) % 10;
+		for (int k = 0; k < 8; k++) {
+			tmp = rt / length;
 
-		String routingNumber = "" + rnd + "" + n9;
+			switch (k) {
+			case 0:tmp *= 3;break;
+			case 1:tmp *= 7;break;
+			case 2:tmp *= 1;break;
+			case 3:tmp *= 3;break;
+			case 4:tmp *= 7;break;
+			case 5:tmp *= 1;break;
+			case 6:tmp *= 3;break;
+			case 7:tmp *= 7;break;
+			}
+
+			rt = rt % length;
+			length = length / 10;
+			n += tmp;
+
+		}
+		if (n % 10 == 0) {
+			n9 = 0;
+		} else {
+			n9 = 10 - n % 10;
+		}
+
+		String routingNumber = "" + random + "" + n9;
 
 		System.out.println(routingNumber);
 
