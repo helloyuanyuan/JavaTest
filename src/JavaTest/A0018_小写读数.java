91bbdaeb8a195fb550ccc4e116c93d7f8208d925
helloyuanyuan <minidisk@vip.qq.com>
2015-05-27 15:51:25 +0800

    20150527 commit

--- src/JavaTest/A0018_小写读数.java
+++ src/JavaTest/A0018_小写读数.java
@@ -4,42 +4,126 @@ public class A0018_Ð¡Ð´¶ÁÊý {
 
 	public static void main(String[] args) {
 		A0018_Ð¡Ð´¶ÁÊý test = new A0018_Ð¡Ð´¶ÁÊý();
-		test.method(4);
+		test.method(-12345);
 	}
 
-	public void method(int a) {
+	public void method(int input) {
 
-		switch (a) {
-		case 1:
-			System.out.println("Ò¼");
-			break;
-		case 2:
-			System.out.println("·¡");
-			break;
-		case 3:
-			System.out.println("Èþ");
-			break;
-		case 4:
-			System.out.println("ËÁ");
-			break;
-		case 5:
-			System.out.println("Îé");
-			break;
-		case 6:
-			System.out.println("Â½");
-			break;
-		case 7:
-			System.out.println("Æâ");
-			break;
-		case 8:
-			System.out.println("°Æ");
-			break;
-		case 9:
-			System.out.println("¾Á");
-			break;
-		default:
-			System.out.println("Áã");
-			break;
+		int rang = 100000;
+
+		if (input >= rang || input <= -rang) {
+			System.out.println("ÄúÊäÈëµÄ: " + input + " ³¬³öÁË·¶Î§¡£ÇëÊäÈë -" + rang + " ~ " + rang + " Ö®¼äµÄÕûÊý£¡");
+		}
+
+		else {
+
+			int output;
+
+			if (input < 0) {
+				System.out.print("¸º ");
+				input *= -1;
+				int lengthA = String.valueOf(input).length();
+				int n = lengthA;
+				int temp = 1;
+
+				while (n > 1) {
+					temp *= 10;
+					n--;
+				}
+				for (int i = 0; i < lengthA; i++) {
+					output = input / temp;
+
+					switch (output) {
+					case 0:
+						System.out.print("Áã ");
+						break;
+					case 1:
+						System.out.print("Ò¼ ");
+						break;
+					case 2:
+						System.out.print("·¡ ");
+						break;
+					case 3:
+						System.out.print("Èþ ");
+						break;
+					case 4:
+						System.out.print("ËÁ ");
+						break;
+					case 5:
+						System.out.print("Îé ");
+						break;
+					case 6:
+						System.out.print("Â½ ");
+						break;
+					case 7:
+						System.out.print("Æâ ");
+						break;
+					case 8:
+						System.out.print("°Æ ");
+						break;
+					case 9:
+						System.out.print("¾Á ");
+						break;
+					}
+
+					input %= temp;
+					temp /= 10;
+				}
+
+			} else if (input > 0) {
+				int lengthA = String.valueOf(input).length();
+				int n = lengthA;
+				int temp = 1;
+
+				while (n > 1) {
+					temp *= 10;
+					n--;
+				}
+
+				for (int i = 0; i < lengthA; i++) {
+					output = input / temp;
+
+					switch (output) {
+					case 0:
+						System.out.print("Áã ");
+						break;
+					case 1:
+						System.out.print("Ò¼ ");
+						break;
+					case 2:
+						System.out.print("·¡ ");
+						break;
+					case 3:
+						System.out.print("Èþ ");
+						break;
+					case 4:
+						System.out.print("ËÁ ");
+						break;
+					case 5:
+						System.out.print("Îé ");
+						break;
+					case 6:
+						System.out.print("Â½ ");
+						break;
+					case 7:
+						System.out.print("Æâ ");
+						break;
+					case 8:
+						System.out.print("°Æ ");
+						break;
+					case 9:
+						System.out.print("¾Á ");
+						break;
+					}
+
+					input %= temp;
+					temp /= 10;
+				}
+
+			} else {
+				System.out.print("Áã ");
+			}
+
 		}
 
 	}
